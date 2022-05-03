73a798e8d69c98baa6b3e214fa3ff677504375ef
Nipunn98 <1998nipunn@gmail.com>
2022-05-03 08:34:45 +0530

    initial commit

---
+++ Data-Storing-Application-main/Home.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Data_Storing_App
+{
+    public partial class Home : Form
+    {
+
+        /*[DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+    (
+        int nLeftRect,
+        int nTopRect,
+        int nRightRect,
+        int nBottomRect,
+        int nWidthEllipse,
+        int nHeightEllipse
+    );*/
+
+        public Home()
+        {
+            InitializeComponent();
+            //Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+            pnlNav.Height = homebtn.Height;
+            pnlNav.Top = homebtn.Top;
+            pnlNav.Left = homebtn.Left;
+            homebtn.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = homebtn.Height;
+            pnlNav.Top = homebtn.Top;
+            pnlNav.Left = homebtn.Left;
+            homebtn.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void formsbtn_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = formsbtn.Height;
+            pnlNav.Top = formsbtn.Top;
+            pnlNav.Left = formsbtn.Left;
+            formsbtn.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void databasebtn_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = databasebtn.Height;
+            pnlNav.Top = databasebtn.Top;
+            pnlNav.Left = databasebtn.Left;
+            databasebtn.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void logoutbtn_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = logoutbtn.Height;
+            pnlNav.Top = logoutbtn.Top;
+            pnlNav.Left = logoutbtn.Left;
+            logoutbtn.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void settingsbtn_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = settingsbtn.Height;
+            pnlNav.Top = settingsbtn.Top;
+            pnlNav.Left = settingsbtn.Left;
+            settingsbtn.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void homebtn_Leave(object sender, EventArgs e)
+        {
+            homebtn.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void formsbtn_Leave(object sender, EventArgs e)
+        {
+            formsbtn.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void databasebtn_Leave(object sender, EventArgs e)
+        {
+            databasebtn.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void logoutbtn_Leave(object sender, EventArgs e)
+        {
+            logoutbtn.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void settingsbtn_Leave(object sender, EventArgs e)
+        {
+            settingsbtn.BackColor = Color.FromArgb(24, 30, 54);
+        }
+    }
+}
